6e50bc06ef67000fe995a7ef9bc6dc7a923e5d70
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2024-10-14 19:00:23 +0200

    setup prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
+  pluginJs.configs.recommended,
+  prettierConfig,
+];
